ecdbc281bd324763c259462ed7a2ca229f84e713
Daniel-Ignacio-Nieto <nieto.ignacio65@gmail.com>
2025-02-28 19:39:45 -0300

    feat(app, styles): add localStorage with superheroes and routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroService } from './shared/services/hero.service';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'super-heroes';
+
+  constructor(private heroService: HeroService) {
+    this.heroService.initializeHeroes();
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,15 @@
 import { Routes } from '@angular/router';
 import { HeroListComponent } from './pages/hero-list/hero-list.component';
+import { HeroFormComponent } from './pages/hero-form/hero-form.component';
 
 export const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'heroes',
-    pathMatch: 'full',
-  },
-  {
     path: 'heroes',
-    component: HeroListComponent,
+    children: [
+      { path: '', component: HeroListComponent },
+      { path: 'form', component: HeroFormComponent },
+    ],
   },
+  { path: '', redirectTo: '/heroes', pathMatch: 'full' },
+  { path: '**', redirectTo: '/heroes' },
 ];
