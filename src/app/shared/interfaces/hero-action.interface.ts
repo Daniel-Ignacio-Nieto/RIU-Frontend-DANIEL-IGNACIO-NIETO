a2da1b144dd59e70ef22ec2d22ce3b7e1a4c055a
Daniel-Ignacio-Nieto <nieto.ignacio65@gmail.com>
2025-03-02 03:09:40 -0300

    feat(models, interfaces, hero.html): change models and interfaces, modify hero html

--- src/app/shared/interfaces/hero-action.interface.ts
+++ src/app/shared/interfaces/hero-action.interface.ts
@@ -1,4 +1,6 @@
+import { Hero } from '../models/hero.model';
+
 export interface HeroActions {
   type: 'edit' | 'delete';
-  id: number;
+  heroe: Hero;
 }
--- src/app/shared/models/hero.model.ts
+++ src/app/shared/models/hero.model.ts
@@ -2,7 +2,7 @@ export class Hero {
   public id: number | null = null;
   public name: string | null = null;
   public alias: string | null = null;
-  public power: string[] = [];
+  public powers: string[] = [];
   public description: string | null = null;
   public imgUrl: string | null = null;
 }
