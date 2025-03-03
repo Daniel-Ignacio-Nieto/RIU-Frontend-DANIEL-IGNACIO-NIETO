9016c288becc3b2c7b9bc1133dc8af047882b362
Daniel-Ignacio-Nieto <nieto.ignacio65@gmail.com>
2025-03-03 04:35:58 -0300

    test: add unitTest for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,28 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HeroService } from './shared/services/hero.service';
+import { RouterOutlet } from '@angular/router';
 
 describe('AppComponent', () => {
+  let mockHeroService: jasmine.SpyObj<HeroService>;
+
   beforeEach(async () => {
+    mockHeroService = jasmine.createSpyObj('HeroService', ['initializeHeroes']);
+
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, RouterOutlet],
+      providers: [{ provide: HeroService, useValue: mockHeroService }],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('should be create component', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    const component = fixture.componentInstance;
+    expect(component).toBeTruthy();
   });
 
-  it(`should have the 'super-heroes' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('super-heroes');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, super-heroes');
+  it('should call initializeHeroes() when instantiated', () => {
+    TestBed.createComponent(AppComponent);
+    expect(mockHeroService.initializeHeroes).toHaveBeenCalled();
   });
 });
